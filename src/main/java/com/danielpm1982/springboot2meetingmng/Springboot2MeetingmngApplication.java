54e494c631aeedda2343798c070ce35d70dbfdde
danielpm1982 <danielpm1982@gmail.com>
2020-02-29 16:58:52 -0300

    Add basic Spring Boot 2 project structure

---
+++ src/main/java/com/danielpm1982/springboot2meetingmng/Springboot2MeetingmngApplication.java
@@ -0,0 +1,10 @@
+package com.danielpm1982.springboot2meetingmng;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2MeetingmngApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2MeetingmngApplication.class, args);
+    }
+}
---
+++ src/test/java/com/danielpm1982/springboot2meetingmng/Springboot2MeetingmngApplicationTests.java
@@ -0,0 +1,10 @@
+package com.danielpm1982.springboot2meetingmng;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2MeetingmngApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
